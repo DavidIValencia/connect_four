ae0b9b96bc49ba00435b29b38fc5d1a4740949a3
Aaron Hu & Sanderfer & Sharon <pair+aaron1515.schaui6.sharonjean@devbootcamp.com>
2016-03-13 13:33:27 -0700

    Test to pull from master

--- application.js
+++ application.js
@@ -1,8 +1,15 @@
+
 var game = function(row, column) {
   var counter = 1
-  // board[row][column] = red || black
+  var color = "none"
+  if(counter % 2 == 0){
+    color = "red";
+    }
+    else{
+      color = "black";
+    };
+  board[row][column] = color
   counter++
+  var board = Array.from(new Array(7), () => Array.from(new Array(6), () => 'a'));
 }
 
-
-var board = Array.from(new Array(7), () => Array.from(new Array(6), () => 'placeholder'));
